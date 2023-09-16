bb4b6bf73315b0b8fd96a55b81adcafbb5ae3adc
bora <nhatbaoxxd@gmail.com>
2023-09-17 03:57:30 +0700

    Create a server using the Express.js library

--- ServerNode.js/index.js
+++ ServerNode.js/index.js
@@ -1,13 +1,18 @@
-const PORT = 4000;
-// Đây là module có sẵn dùng để tạo server Node.js
-const http = require('http');
-console.log(http);
+// Lấy express từ trong require express ra
+const express = require('express')
+// Gọi express function và gắn vào biến app
+const app = express()
+const port = 3000
 
-const server = http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.end('{"message": "Hello world"}');
-});
+// Khai báo router
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.get('/hi', (req, res) => {
+  res.send('Xin chào thế giới!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
