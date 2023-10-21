6260e5ee821a84db512da95af28242cffac6e093
bora <nhatbaoxxd@gmail.com>
2023-10-21 07:47:22 +0700

    Create your first server with Node.js

---
+++ ServerNode-review.js/main.js
@@ -0,0 +1,12 @@
+// Đây là module có sẵn dùng để tạo server Node.js
+const http = require('http')
+const PORT = 4000
+
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+  res.end(`{"message": "Hello World"}`)
+})
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
