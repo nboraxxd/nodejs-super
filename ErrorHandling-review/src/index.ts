2961b259c5bbc37bb033353799ae33180f45f58a
bora <nhatbaoxxd@gmail.com>
2023-10-22 10:20:55 +0700

    Error handling

---
+++ ErrorHandling-review/src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+const sum = (obj: { a: number; b: number }) => {
+  return obj.a + obj.b
+}
+
+app.get('/', (req, res) => {
+  const value = sum({ a: 1, b: 2 })
+
+  res.send(`Hello World!, ${value}`)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
